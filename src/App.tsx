eeeaceed3f643225ffd3f75eadd5a758fba00b41
Mohamed-lehbib <abeidnamohamedlehbib@gmail.com>
2024-03-06 12:52:25 +0000

    adding the email auth and SignInButton

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Link } from "react-router-dom";
+import { SignInButton } from "@clerk/clerk-react";
 
 function App() {
   return (
     <div className="App">
-      <Link to="/sign-in">Signin</Link>
+      {/* <Link to="/sign-in">Signin</Link> */}
+      <SignInButton />
     </div>
   );
 }
