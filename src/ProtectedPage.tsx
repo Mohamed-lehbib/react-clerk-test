1496ee7dd4a09274e6912aa1efefb9ac17e9ac27
Mohamed-lehbib <abeidnamohamedlehbib@gmail.com>
2024-03-06 12:18:22 +0000

    adding the authentication using clerk

---
+++ src/ProtectedPage.tsx
@@ -0,0 +1,6 @@
+import { UserButton } from "@clerk/clerk-react";
+import React from "react";
+
+export default function ProtectedPage() {
+  return <UserButton />;
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,78 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import {
+  BrowserRouter,
+  Route,
+  Router,
+  Routes,
+  useNavigate,
+} from "react-router-dom";
+import {
+  ClerkProvider,
+  RedirectToSignIn,
+  SignIn,
+  SignUp,
+  SignedIn,
+  SignedOut,
+} from "@clerk/clerk-react";
+import ProtectedPage from "./ProtectedPage";
+
+const PUBLISHABLE_KEY = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
+
+const ClerkWithRoutes = () => {
+  const navigate = useNavigate();
+  return (
+    <ClerkProvider
+      publishableKey={PUBLISHABLE_KEY}
+      navigate={(to) => navigate(to)}
+    >
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route
+          path="/sign-in/*"
+          element={
+            <SignIn redirectUrl={"/protected"} routing="path" path="/sign-in" />
+          }
+        />
+        <Route
+          path="/sign-up/*"
+          element={
+            <SignUp redirectUrl={"/protected"} routing="path" path="/sign-up" />
+          }
+        />
+        <Route
+          path="/protected"
+          element={
+            <>
+              <SignedIn>
+                <ProtectedPage />
+              </SignedIn>
+              <SignedOut>
+                <RedirectToSignIn />
+              </SignedOut>
+            </>
+          }
+        />
+      </Routes>
+    </ClerkProvider>
+  );
+};
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ClerkWithRoutes />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
